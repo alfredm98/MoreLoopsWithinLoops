7e72bb47e08d0a9fe4471d93873eee916522be8f
mustafa <mustafa@rose-hulman.edu>
2019-07-23 23:39:58 -0400

    More Loops within Loops

--- src/m3_more_nested_loops_in_sequences.py
+++ src/m3_more_nested_loops_in_sequences.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of SEQUENCES OF SUB-SEQUENCES.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Alfred Mustafa.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_largest_number():
     """ Tests the    largest_number    function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  largest_number  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond those we wrote.
     # ------------------------------------------------------------------
@@ -44,6 +44,16 @@ def run_test_largest_number():
     print('Expected and actual are:', expected, answer)
 
     # TO DO 2 (continued): Add your ADDITIONAL test(s) here:
+
+    # Test 4:
+    expected = 2
+    answer = largest_number(([2], [1], [0]))
+    print('Expected and actual are:', expected, answer)
+
+    # Test 5:
+    expected = 44444
+    answer = largest_number(([34], [44444], [1]))
+    print('Expected and actual are:', expected, answer)
 
 
 def largest_number(seq_seq):
@@ -72,15 +82,26 @@ def largest_number(seq_seq):
     where each subsequence contains only numbers.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    seq = []
+    for k in range(len(seq_seq)):
+        for j in range(len(seq_seq[k])):
+            seq = seq + [seq_seq[k][j]]
+    if seq == []:
+        return None
+    k_max = 0
+    for k in range(0, len(seq)):
+        if seq[k] > seq[k_max]:
+            k_max = k
+    return seq[k_max]
 
 
 def run_test_largest_negative_number():
     """ Tests the    largest_negative_number    function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # DONE: 4. Implement this TEST function.
     #   It TESTS the  largest_negative_number  function defined below.
     #
     #   Include enough tests to give you confidence that your solution
@@ -90,6 +111,34 @@ def run_test_largest_negative_number():
     print('-------------------------------------------------')
     print('Testing the   LARGEST_NEGATIVE_NUMBER   function:')
     print('-------------------------------------------------')
+    # Test 1:
+    expected = -1
+    answer = largest_negative_number([(-3, -1, 4),
+                             (-13, 10, 11, 7, 10),
+                             [1, -2, 3, 4]])
+    print('Expected and actual are:', expected, answer)
+
+    # Test 2:
+    expected = -1111111111111111
+    answer = largest_negative_number(([], [-1111111111111111], []))
+    print('Expected and actual are:', expected, answer)
+
+    # Test 3:
+    expected = None
+    answer = largest_negative_number(([], [], []))
+    print('Expected and actual are:', expected, answer)
+
+    # Test 4:
+    expected = None
+    answer = largest_negative_number(([2], [1], [0]))
+    print('Expected and actual are:', expected, answer)
+
+    # Test 5:
+    expected = -44444
+    answer = largest_negative_number(([34], [-44444], [1]))
+    print('Expected and actual are:', expected, answer)
+
+
 
 
 def largest_negative_number(seq_seq):
@@ -115,13 +164,33 @@ def largest_negative_number(seq_seq):
     where each subsequence contains only numbers.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # CHALLENGE: Try to solve this problem with no additional sequences
     #   being constructed (so the SPACE allowed is limited to the
     #   give sequence of sequences plus any non-list variables you want).
     # ------------------------------------------------------------------
+    seq = []
+    for k in range(len(seq_seq)):
+        for j in range(len(seq_seq[k])):
+            seq = seq + [seq_seq[k][j]]
+    if seq == []:
+        return None
+
+    seq2 = []
+    for k in range(len(seq)):
+        if seq[k] < 0:
+            seq2 = seq2 + [seq[k]]
+
+    if seq2 == []:
+        return None
+
+    k_max = 0
+    for k in range(0, len(seq2)):
+        if seq2[k] > seq2[k_max]:
+            k_max = k
+    return seq2[k_max]
 
 
 def run_test_first_is_elsewhere_too():
@@ -355,7 +424,7 @@ def first_is_elsewhere_too(seq_seq):
     and the given argument is a sequence of sequences.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -370,6 +439,18 @@ def first_is_elsewhere_too(seq_seq):
     #   in this problem, as doing so would defeat the goal of providing
     #   practice at loops within loops (within loops within ...)
     # ------------------------------------------------------------------
+    first = 0
+    if len(seq_seq) == 0:
+        return False
+    if len(seq_seq) == 1:
+        return False
+    for i in range(len(seq_seq[0])):
+        first = seq_seq[0][i]
+        for k in range(len(seq_seq)):
+            for j in range(len(seq_seq[k])):
+                if seq_seq[k][j] == first and k != 0:
+                    return True
+    return False
 
 
 # ----------------------------------------------------------------------
